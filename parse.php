d7b8cf7c336cf4678a4bbe320a17c1c90315dea3
Brian Ho <brian@codeforart.com>
2013-04-12 20:40:27 -0700

    New schema and Parsing for resets
    updated ignore file

--- parse.php
+++ parse.php
@@ -4,51 +4,42 @@ include_once("db.php");
 <!doctype html>
 <html lang="utf8">
     <body>
-        <?php
-        if (isset($argv[1]))
-            $area = $argv[1];
-        else
-            $area = 'D:\\thelostatlantis\\area\\area.lst';
+        <pre>
+<?php
+            if (isset($argv[1]))
+                $area = $argv[1];
+            else
+                $area = 'D:\\thelostatlantis\\area\\area.lst';
 
-        try {
-            $pdo = new PDO(
-                    'mysql:dbname=la;host=localhost;charset=utf8',
-                    'root',
-                    null,
-                    array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
-                    ));
-        } catch (PDOException $e) {
-            print_r($e);
-            return;
-        }
+            try {
+                $pdo = new PDO(
+                        'mysql:dbname=la;host=localhost;charset=utf8', 'root', null, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+                ));
+            } catch (PDOException $e) {
+                print_r($e);
+                return;
+            }
 
-        $stmt = $pdo->prepare('delete from object');
-        $stmt->execute() or die($stmt->errorCode());
+            $tables = array('obj_in_mob_equip', 'obj_in_obj', 'obj_in_room', 'mob_in_room', 'mob', 'object', 'object_affects', 'object_ed', 'resets', 'room', 'room_exits');
+            foreach ($tables as $table) {
+                $stmt = $pdo->prepare('delete from ' . $table);
+                $stmt->execute() or die(print_r($stmt->errorInfo(), true));
+            }
 
-        $stmt = $pdo->prepare('delete from object_affects');
-        $stmt->execute() or die($stmt->errorCode());
+            $dir = dirname($area) . '\\';
+//            dump_area($dir . 'air.are'); return;
 
-        $stmt = $pdo->prepare('delete from room');
-        $stmt->execute() or die($stmt->errorCode());
+            $areas = file($area);
+            foreach ($areas as $area_file) {
+                $area_file = trim($area_file);
+                var_dump($area_file);
 
-        $stmt = $pdo->prepare('delete from room_exits');
-        $stmt->execute() or die($stmt->errorCode());
+                if (preg_match('/\w*\.are/', $area_file))
+                    dump_area($dir . $area_file);
+            }
 
-        $dir = dirname($area) . '\\';
-        dump_area($dir . 'limbo.are');
-        return;
-
-        $areas = file($area);
-        foreach ($areas as $area_file) {
-            $area_file = trim($area_file);
-            var_dump($area_file);
-
-            if (preg_match('/\w*\.are/', $area_file))
-                dump_area($dir . $area_file);
-        }
-
-        echo 'done';
-        ?>
-
+            echo 'done';
+            ?>
+        </pre>
     </body>
 </html>
